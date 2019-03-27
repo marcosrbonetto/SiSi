0d4fa4336ddf93f5ab542839e71a8efa247c4bd1
Adrian Dotta <adridotta@gmail.com>
2019-03-27 15:56:39 -0300

    [TO-1] Correcciones del maldito Fede

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 const DEPLOY = 1;
 const TEST = 2;
 const LOCAL = 3;
-const ENTORNO = LOCAL;
+const ENTORNO = DEPLOY;
 
 //Url Root
 const URL_ROOT_SISI_LOCAL = "http://localhost:3000/#";
@@ -16,9 +16,9 @@ const URL_WS_SISI_DEPLOY = "https://servicios2.cordoba.gov.ar/WSSiSi_Bridge";
 let URL_WS_SISI = URL_WS_SISI_DEPLOY;
 
 //Url Login
-const URL_LOGIN_LOCAL = "https://servicios2.cordoba.gov.ar/VecinoVirtual/Login/#/Login/SiSiPresencialLocal";
-const URL_LOGIN_TEST = "https://servicios2.cordoba.gov.ar/VecinoVirtual/Login/#/Login/SiSiPresencialTest";
-const URL_LOGIN_DEPLOY = "https://servicios2.cordoba.gov.ar/VecinoVirtual/Login/#/Login/SiSiPresencial";
+const URL_LOGIN_LOCAL = "https://servicios2.cordoba.gov.ar/MuniOnlineLogin/#/Login/SiSiPresencialLocal";
+const URL_LOGIN_TEST = "https://servicios2.cordoba.gov.ar/MuniOnlineLogin/#/Login/SiSiPresencialTest";
+const URL_LOGIN_DEPLOY = "https://servicios2.cordoba.gov.ar/MuniOnlineLogin/#/Login/SiSiPresencial";
 let URL_LOGIN = URL_LOGIN_DEPLOY;
 
 //Segun el entorno, cargo las variables
@@ -60,7 +60,7 @@ var Config = {
     "f_qdag0f9irgka9xj2l6mbll69gxmhlghezkmkj2mykg1pj0uuhwogqiqfic_c327l9gmyk9tutz1fuq0rc3_z2byq5gcg2j5tjpqcn6jid4x2rlv2nsaa2it7s64d7m2k4h7e_xegt2w8p79uvk4jj42a7uvrcfm1cn8jpq31o4raxvsv8ktwtsa_q6iqbxeop56c_zee",
   IDENTIFICADOR_FOTO_DEFAULT_FEMALE:
     "f_zq38nzky73iwxm6fz4m812vx68ggr28xgokqfwx7zf9ws7rd6_s7mn985gcqtehf6vpicq_chqiv3_e9rdlsjal4pmw_uhnu9318riap_p07eoe5cd_q4z65kw304ataczwaihsic6t4lo0bh18qi81k86x6qlv_7z5q2ew6w1n8gbu772sdcd3e8mcnuw31ku8wtkkd",
-  URL_MI_PERFIL: "https://servicios2.cordoba.gov.ar/VecinoVirtual/Utils/MiPerfil",
+  URL_MI_PERFIL: "https://servicios2.cordoba.gov.ar/MuniOnlinePerfil",
   TOKEN_INVITADO: 'INVITADO',
   EMAIL_SOPORTE: 'soporte@cordoba.gov.ar'
 };
